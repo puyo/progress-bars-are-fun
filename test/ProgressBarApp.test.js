e523f21399393675c19c17b945d5dcb1a55d9a9c
Gregory McIntyre <greg@gregorymcintyre.com>
2016-11-23 15:49:03 +1100

    Use chai-enzyme for asserts

--- test/ProgressBarApp.test.js
+++ test/ProgressBarApp.test.js
@@ -1,15 +1,13 @@
 import React from 'react'
-import { expect } from 'chai'
 import { render, mount, shallow } from 'enzyme'
 import ProgressBarsApp from '../src/ProgressBarsApp'
 import Bar from '../src/Bar'
 
-const testFetch = Promise.resolve({
-    bars: [],
-    buttons: [],
-    limit: [],
-    selected: []
-})
+import chai from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+
+chai.use(chaiEnzyme()) // Note the invocation at the end
+const expect = chai.expect
 
 describe('<ProgressBarsApp />', () => {
     before(function() {
@@ -28,5 +26,9 @@ describe('<ProgressBarsApp />', () => {
 
     it('contains the right number of buttons', function() {
         expect(this.wrapper.find('button')).to.have.length(3)
+    })
+
+    it('contains the right labels on buttons', function() {
+        expect(this.wrapper.find('button').first()).to.have.text('5')
     })
 })
